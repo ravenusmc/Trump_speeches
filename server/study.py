1fd2d2a1f3f702ffec21da367fcad5a3680be1d7
ravenusmc <mcuddy77@gmail.com>
2020-09-07 18:42:38 -0400

    working on sorting data

--- server/study.py
+++ server/study.py
@@ -12,17 +12,23 @@ from pathlib import Path
 class textAnalysis():
 
     def __init__(self):
-        #pass
         self.first_speech = pd.read_json('./data/one.json', typ='series')
         self.second_speech = pd.read_json('./data/two.json', typ='series')
+        self.third_speech = pd.read_json('./data/three.json', typ='series')
+        self.fourth_speech = pd.read_json('./data/four.json', typ='series')
+        self.fifth_speech = pd.read_json('./data/five.json', typ='series')
         self.sixth_speech = pd.read_json('./data/six.json', typ='series')
+        self.seventh_speech = pd.read_json('./data/seven.json', typ='series')
+        self.eigth_speech = pd.read_json('./data/eight.json', typ='series')
         self.ninth_speech = pd.read_json('./data/nine.json', typ='series')
-        #self.speeches = pd.read_json("./data/test.json", lines = True)
+        self.tenth_speech = pd.read_json('./data/ten.json', typ='series')
+        self.eleventh_speech = pd.read_json('./data/eleven.json', typ='series')
+        self.twelve_speech = pd.read_json('./data/twelve.json', typ='series')
 
     def working(self):
         # data = json.load(open('./data/trumpspeeches.json'))
         # print(type(data))
-        print(self.ninth_speech['speechlocation'])
+        print(self.second_speech['speechlocation'])
         # with Path("./data/test.json").open("r") as f:
         #     for line in f:
         #         try:
