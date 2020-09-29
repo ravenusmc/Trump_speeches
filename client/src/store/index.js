e56b304aa7b153693e7f34e5dbc5ffa8b73e4821
ravenusmc <mcuddy77@gmail.com>
2020-09-29 07:37:20 -0400

    working on getting data over to server

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-// import axios from 'axios';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
@@ -16,18 +16,15 @@ export default new Vuex.Store({
   actions: {
 
     // This action will get the data for the speech selected. Remember that
-    // the action appears to NEED to take 2 arguments, not 1. 
+    // the action appears to NEED to take 2 arguments, not 1.
     fetchOneSpeechChartData: ({ commit }, { payload }) => {
-      console.log('In Action');
-      console.log(payload.speech);
-      const data = 'Mike';
-      commit('setOneSpeechChartData', data);
-      // const path = 'http://localhost:5000/routeOne';
-      // axios.post(path, payload)
-      // .then((res) => {
-      //   res.data.sort((a, b) => b[1] - a[1]);
-      //   commit('setFirstChartData', res.data);
-      // });
+      const path = 'http://localhost:5000/single_speech_chart';
+      axios.post(path, payload)
+        .then((res) => {
+          console.log(res);
+          // res.data.sort((a, b) => b[1] - a[1]);
+          commit('setOneSpeechChartData', res.data);
+        });
     },
 
   },
--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+#importing code that I wrote
+# from data import *
 
 # configuration
 DEBUG = True
@@ -10,12 +12,15 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 # enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app)
+# CORS(app, resources={r'/*': {'origins': '*'}})
 
-
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
+# This route will get the data for the first chart.
+@app.route('/single_speech_chart', methods=['GET', 'POST'])
+def single_speech_chart():
+    if request.method == 'POST':
+        post_data = request.get_json()
+        print(post_data)
     return jsonify('pong!')
 
 
