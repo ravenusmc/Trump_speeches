d0b1a5126aa5a3d2fb336df07a06f3e2ea15c5a7
ravenusmc <mcuddy77@gmail.com>
2020-10-09 06:35:38 -0400

    working on design of sentence sentiment area

--- client/src/components/studyComponents/OneSpeech.vue
+++ client/src/components/studyComponents/OneSpeech.vue
@@ -1,57 +1,31 @@
 <template>
   <div>
 
-    <section id='card first-chart-area'>
-
-      <div class='card-body'>
-        <!-- Bootstrap From -->
-        <form @submit="submitForm">
-          <div class="form-group">
-            <label for="exampleInputEmail1"><span>Select Speech:</span></label>
-            <select class="custom-select" v-model="speech" name="speech">
-              <option v-for="speech in speeches" v-bind:key="speech" :value="speech">
-                {{ speech }}
-              </option>
-            </select>
-          </div>
-          <button type="submit" class="btn btn-outline-dark">Submit</button>
-        </form>
-        <!-- End Boostrap Form -->
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">
+          Some quick example text to build on the card title
+          and make up the bulk of the card's content.
+        </p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
       </div>
+    </div>
 
-      <div class='card-body'>
-        <div class='average-sentiment-div'>
-          <p><span>Average Sentiment in Speech:</span> {{ this.speechMean }}</p>
-          <div class='sentenceArea'>
-            <p><span>Current Sentence:</span> {{ this.selectedSentence }}</p>
-          </div>
-          <p>Sentence Sentiment: {{ this.sentenceSentiment }}</p>
-          <h4>Change Sentence:</h4>
-          <div>
-            <svg
-            @click="changeSentence('up')"
-            width="3em"
-            height="3em"
-            viewBox="0 0 16 16"
-            class="bi bi-arrow-up-circle-fill svgLeft"
-            fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-              d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354
-              7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5
-              5.707V11.5z"/>
-            </svg>
-            <svg @click="changeSentence('down')" width="3em" height="3em" viewBox="0 0 16 16"
-              class="bi bi-arrow-down-circle-fill svgRight"
-              fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1
-              0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0
-              0-.708-.708L8.5 10.293V4.5z"/>
-            </svg>
-          </div>
-        </div>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">
+          Some quick example text to build on the card title
+          and make up the bulk of the card's content.
+        </p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
       </div>
-
-    </section>
+    </div>
 
   </div>
 </template>
@@ -112,16 +86,16 @@ export default {
     },
     changeSentence(direction) {
       let value = this.initalValue;
+      if (direction === 'up') {
+        value += 1;
+      } else if (direction === 'down') {
+        value -= 1;
+      }
       if (value > this.speechLength) {
         alert('The end of the speech has been reached!');
       } else if (value < 0) {
         alert('You cannot go back any further');
       } else {
-        if (direction === 'up') {
-          value += 1;
-        } else if (direction === 'down') {
-          value -= 1;
-        }
         const payload = {
           value,
           speech: this.selectedSpeech,
@@ -163,10 +137,10 @@ span {
 }
 
 .average-sentiment-div {
-  /* display: flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center; */
+  align-items: center;
 }
 
 </style>
---
+++ client/src/components/studyComponents/Play.vue
@@ -0,0 +1,52 @@
+<section id='card'>
+
+  <div class='card-body first-chart-area'>
+
+    <!-- Bootstrap From  -->
+    <form @submit="submitForm">
+      <div class="form-group">
+        <label for="exampleInputEmail1"><span>Select Speech:</span></label>
+        <select class="custom-select" v-model="speech" name="speech">
+          <option v-for="speech in speeches" v-bind:key="speech" :value="speech">
+            {{ speech }}
+          </option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-outline-dark">Submit</button>
+      <p><span>Average Sentiment of Speech:</span> {{ this.speechMean }}</p>
+    </form>
+    <!-- End Boostrap Form -->
+  </div>
+
+  <div class='card-body'>
+    <div class='average-sentiment-div'>
+      <div class='sentenceArea'>
+        <p><span>Current Sentence:</span> {{ this.selectedSentence }}</p>
+      </div>
+      <p>Sentence Sentiment: {{ this.sentenceSentiment }}</p>
+      <h4>Change Sentence:</h4>
+      <div>
+        <svg
+        @click="changeSentence('up')"
+        width="3em"
+        height="3em"
+        viewBox="0 0 16 16"
+        class="bi bi-arrow-up-circle-fill svgLeft"
+        fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd"
+          d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354
+          7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5
+          5.707V11.5z"/>
+        </svg>
+        <svg @click="changeSentence('down')" width="3em" height="3em" viewBox="0 0 16 16"
+          class="bi bi-arrow-down-circle-fill svgRight"
+          fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1
+          0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0
+          0-.708-.708L8.5 10.293V4.5z"/>
+        </svg>
+      </div>
+    </div>
+  </div>
+
+</section>
